f06c9cf545821a074e51dd9203ae27b740c0d4c1
Sara McMullin <mcsara1221@gmail.com>
2019-06-25 23:56:58 -0500

    contact page

--- app.js
+++ app.js
@@ -4,3 +4,8 @@ $(".dropdown-trigger").dropdown();
   $(document).ready(function(){
     $('.carousel').carousel();
   });
+
+  $(document).ready(function(){
+    $('.parallax').parallax();
+  });
+      
